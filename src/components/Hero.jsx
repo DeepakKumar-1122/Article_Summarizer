c39073ad248572bd8340bd35bbafd2e82e33651e
Deepak <deepakmuthu1122@gmail.com>
2024-02-08 21:33:45 +0530

    Link Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/DeepakKumar-1122/Article_Summarizer", "_blank")
           }
           className='black_btn'
         >
